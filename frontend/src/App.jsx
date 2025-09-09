916a581d996911f50c1e86b6bb8c90c235635719
Ajay <jaykumar002kori@gmail.com>
2025-09-09 12:55:29 +0530

    feat: enhance landing page layout and styling with new components and CSS

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,9 @@
-import './App.css'
-import Header from './components/header.jsx'
+
 import { Route, Routes} from "react-router-dom"
-import Home from './components/home.jsx' 
-import About from './components/about.jsx'
-import Contact from './components/contact.jsx'
 
 function App() {
       
-  return (
-    <div>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </div>
-  )
+  return null
 }
 
 export default App
