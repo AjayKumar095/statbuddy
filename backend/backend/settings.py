7beda116f88f423ddfbd065eb631bf6e857b46aa
Ajay <jaykumar002kori@gmail.com>
2025-09-09 09:47:27 +0530

    feat: add authenticator and root_app modules with initial setup

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -32,7 +32,8 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
      "corsheaders", # for handling CORS
-     "root", # our new app
+     "authenticator",
+      "root_app", # our new app
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
